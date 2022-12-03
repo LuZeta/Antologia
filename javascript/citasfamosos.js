442a254a8846da5b1d569bf8799645a7b6911ad3
Alicia Zuliani <luzul2424@gmail.com>
2022-12-03 10:51:29 -0300

    quotes_citasFamosos

---
+++ javascript/citasfamosos.js
@@ -0,0 +1,27 @@
+const divCitas = document.querySelector("#citaFamoso_div");
+
+let quotes = [];
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4b7ea05b3fmsh3018449e36ef6a5p14ff00jsn9ce3e251f2ff",
+    "X-RapidAPI-Host": "quotes15.p.rapidapi.com",
+  },
+};
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", options)
+  .then((response) => response.json())
+  .then((data) => {
+    quotes = data;
+console.log(quotes);
+    quotes.forEach((quotes) => {
+      const div = document.createElement("div");
+      div.innerHTML = `
+       <p>${quotes.content}</p>`;
+
+      divCitas.append(div);
+      console.log(div);
+    });
+  })
+  .catch((err) => console.error(err));
